1e3cadfbacbe4a3611172923a90bb3e0cf6c3e34
root <oyerooluwakemisola@gmail.com>
2023-02-20 21:00:27 -0800

    new files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints in lower case in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'z'; al <= 'a'; al--)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
