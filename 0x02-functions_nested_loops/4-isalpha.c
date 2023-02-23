9fd0fe7d34c10a30bd39a45dbfe1ed95debfeb60
root <oyerooluwakemisola@gmail.com>
2023-02-22 20:43:07 -0800

    fix docs erroe

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - a function that checks for alphabetic character
  *@c: the character to be checked
  *
- * Returns: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
