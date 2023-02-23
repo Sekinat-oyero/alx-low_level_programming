ac0f382426eabdc264eca26b202b3da14998c546
root <oyerooluwakemisola@gmail.com>
2023-02-22 18:33:11 -0800

    fix errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char al;
 	int i;
 
-	for (i = 1; i = 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (al = 'a'; al = 'z'; al++)
+		for (al = 'a'; al <= 'z'; al++)
 		_putchar(al);
 		_putchar('\n');
 	}
