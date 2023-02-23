f697ff85c825eb19e68e75c1ffbdccaf3498cf38
root <oyerooluwakemisola@gmail.com>
2023-02-22 19:16:00 -0800

    fixing betty error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
  * _islower - a function that checks for lowercase character
+ *@c: the character to be checked
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 
 /**
  * _isalpha - a function that checks for alphabetic character
+ *@c: the character to be checked
  *
  * Returns: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
