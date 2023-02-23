0cdc0de707e670859e463324f33b4670d093dacc
root <oyerooluwakemisola@gmail.com>
2023-02-22 18:24:01 -0800

    function that prints alpbabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabets
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char al;
+	int i;
+
+	for (i = 1; i = 10; i++)
+	{
+		for (al = 'a'; al = 'z'; al++)
+		_putchar(al);
+		_putchar('\n');
+	}
+}
